96ecc4b3132ae4975115740492d58b7353db25bf
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2021-12-24 02:44:50 +0800

    Started setup, added folders to categorize items, added 1 pages and routes to the user and places.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router, Route,Redirect,Switch} from 'react-router-dom'
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace'
+
+function App() {
+  return (
+  <Router>
+    <Switch>
+      <Route exact path="/"><Users/></Route>  
+      <Route path="/places/new" exact>
+        <NewPlace/>
+      </Route>
+      <Redirect to="/"></Redirect>
+    </Switch>
+ 
+  </Router>
+  )
+}
+
+export default App;
