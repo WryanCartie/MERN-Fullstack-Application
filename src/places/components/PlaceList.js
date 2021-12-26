4c069685bfcae4b26aaa868abb0e8ac28f109566
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2021-12-27 01:01:11 +0800

    PlaceItem and PlaceList added(unfinished)

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Card from '../../shared/components/UIElements/Card'
+
+const PlaceList = props => {
+    if(props.items.length === 0){
+        return <div className="place-list center">
+            <Card>
+                <h2>No places found. Want to create one?</h2>
+                <button>Share your places with us!</button>
+            </Card>
+        </div>
+    }
+}
+return <ul className='place-list'>
+    {props.items.map(place => 
+    <PlaceItem key={place.id}/>)}
+</ul>
