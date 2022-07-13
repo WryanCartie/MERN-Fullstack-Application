c85555be223104ddbfcfa98635bfe99d93e4aab8
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-07-13 15:48:14 +0800

    Implement the users to fetch data from db.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 
 import UsersList from "../components/UsersList";
 
-const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Max Schwarz",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3,
-    },
-  ];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-  return <UsersList items={USERS} />;
+const Users = () => {
+  const[isLoading,setLoading] = useState(false)
+  const[error, setError] = useState();
+  const[loadedUsers,setLoadedUsers] = useState()
+
+  useEffect(()=>{
+    const sendRequest = async ()=>{
+      setLoading(true)
+      try{
+        const response = await fetch('http//localhots:5000/api/users')
+        const responseData = response.json()
+        setLoadedUsers(responseData.users)
+        if(!response.ok){
+          throw new Error(responseData.messages)
+        }
+      }catch(err){
+        setError(err.message)
+
+      }
+    
+      setLoading(false)
+    }
+    sendRequest();
+  })
+
+  const errorHandler = () =>{
+    setError(null);
+  }
+
+
+  return (
+  <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler}/>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner/>
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={USERS} />}
+  </React.Fragment>
+
+
+  )
 };
 
 export default Users;
