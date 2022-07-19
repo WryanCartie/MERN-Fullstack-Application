56c4bbdb45af497c6828a15831563aae4f5b89fb
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2022-07-19 15:14:05 +0800

    Fix error handling and abort ctrl error in costum hooks.

--- src/shared/hooks/http-hooks.js
+++ src/shared/hooks/http-hooks.js
@@ -1,4 +1,5 @@
 import {useState, useCallback,useRef} from 'react'
+import { useEffect } from 'react/cjs/react.production.min';
 
 export const useHttpClient = () =>{
     const [isLoading, setLoading] = useState(false);
@@ -15,20 +16,32 @@ export const useHttpClient = () =>{
                 headers,
                 signal: httpAbortCtrl.signal
             })
-            const responseData = response.json()
+            const responseData = await response.json()
+
+            activeHttpRequest = activeHttpRequest.filter(requestCtrl => requestCtrl !== httpAbortCtrl)
+
             if(!response.ok){
                 throw Error(responseData.message)
             }
+            setIsLoading(false)
             return responseData
         }catch(error){
             setError(error.message)
+            setIsLoading(false)
+            throw err
         }
-        setIsLoading(false)
       
     },[])
 
     const clearError = () =>{
         setError(null);
     }
+
+    useEffect(()=>{
+        return () =>{
+            activeHttpRequest.current.forEach(abortCtrl=> abortCtrl.abort())
+        }
+
+    },[])
     return isLoading,error,sendRequest,clearError
 }
