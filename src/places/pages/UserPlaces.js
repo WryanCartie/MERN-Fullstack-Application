7359cdfd753c16be73b9a217def4be3553c762f2
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-07-26 12:28:06 +0800

    Implement userPlaces using costum hook.

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 
 import PlaceList from "../components/PlaceList";
 import { useParams } from "react-router-dom";
+import { useHttpClient } from "../../shared/hooks/http-hooks";
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Stockholm Palace",
-    description:
-      "Palace hosting the emperors and empresses of the Swedish Empire.",
-    imageUrl:
-      "https://media.istockphoto.com/photos/royal-palace-in-stockholm-hdr-picture-id641802618?k=20&m=641802618&s=612x612&w=0&h=axGbCNMymi9xavDpcmlXyPk8inWULfoWf3NIeF7PEkU=",
-    address: "Kungliga slottet, 107 70 Stockholm, Sweden",
-    location: {
-      lat: 59.3268,
-      lng: 18.0717,
-    },
-    creator: "u1",
-  },
-];
+
+
 
 const UserPlaces = () => {
-  const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+  const userId = useParams.userId;
+  const {sendRequest,isLoading,error,clearError} = useHttpClient
+  const[loadedPlace,setLoadedPlace] = useState([]);
+  useEffect(()=>{
+    const fetchPlaces = async ()=>{
+      try{
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        console.log(responseData);
+        setLoadedPlace(responseData.places)
+   
+      }catch(err){
+      
+      }
+      fetchPlaces()
+    }
+  },[sendRequest,userId])
+  return <PlaceList items={loadedPlace} />;
 };
 
 export default UserPlaces;
