d4bcf3e2f3815286d826c8baf12e61d019a5a677
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-30 15:35:37 +0800

    Saved token in localStorage.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ const App = () => {
   const login = useCallback((uid,token) => {
     setToken(token)
     setUserId(uid);
+    localStorage.setItem('userData',JSON.stringify({
+      userId: uid,
+      token: token
+    }))
   }, []);
   const logout = useCallback(( ) => {
     setToken(null)
